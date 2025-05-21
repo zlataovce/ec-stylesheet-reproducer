64c72af265e937e2b2f34ff4422ab46cb76446f1
Matouš Kučera <mk@kcra.me>
2025-05-21 15:56:15 +0200

    feat: make reproducer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: "Starlight",
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				{ label: "With code block", link: "/reproducer/working" },
+				{ label: "Without code block", link: "/reproducer/not-working" },
 			],
+			expressiveCode: {
+				emitExternalStylesheet: false,
+			},
 		}),
 	],
 });
---
+++ src/content/docs/index.md
@@ -0,0 +1,6 @@
+---
+title: EC stylesheet bug reproducer
+---
+
+* [With code block](/reproducer/working)
+* [Without code block](/reproducer/not-working)
